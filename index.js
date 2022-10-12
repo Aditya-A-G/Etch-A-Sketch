4defd33baebdb6f4b06ba5e63575e3093440356f
Aditya Ghadge <adityaghadge360@gmail.com>
2022-10-12 20:39:20 +0530

    Change grid elements color on hover

--- index.js
+++ index.js
@@ -9,35 +9,62 @@ btn.addEventListener("click", generateGrid);
 
 createGrid(20);
 
+
 function generateGrid(){
-    console.log(input.value)
-    container.remove()
+
+    container.remove();
+   
     createGrid(input.value);
+    
     input.value = '';
-    console.log(container)
+
 }
 
+function generateRandomNumber(){
+    
+    let num = Math.floor(Math.random() * 7);
+    
+    return num;
+
+}
+
+function changeColor(e){
+    
+    let arr= ["rgb(255, 0, 0)", "rgb(255, 165, 0)", "rgb(255, 255, 0)", "rgb(0, 128, 0)", "rgb(0, 0, 255)", "rgb(75, 0, 130)", "rgb(238, 130, 238)"];
+
+    let index = generateRandomNumber();
+
+    e.target.style.backgroundColor = arr[index];
+
+}
+
+
 function createGrid(size){
-    console.log(size);
+    
     let totalSize = size*size;
     let dimension = 400/size;
 
     container = document.createElement("div");
+
     container.style.cssText = ` height: 400px;
                                 width: 400px;
                                 display: flex;
                                 flex-wrap: wrap;`;
+
     mainDiv.appendChild(container); 
-    
 
     for(let i = 0; i<totalSize; i++){
 
         div = document.createElement("div");
+
         div.style.cssText = `height: ${dimension}px; 
                              width: ${dimension}px; 
                              background-color: rgb(100%, 100%, 100%);
                              border: 1px solid red`;
+                             
         container.appendChild(div);
+
+        div.addEventListener("mouseover", changeColor);
 
     }
 }
