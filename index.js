2282596dd3b61cb9e41cf732f29581a18d967287
Aditya Ghadge <adityaghadge360@gmail.com>
2022-10-16 16:09:52 +0530

    Reduce color brightness after each pass

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 let container = 0;
 let previousNum = 7;
+let obj = {};
 
 const input = document.getElementById("input");
 const mainDiv = document.querySelector(".main");
@@ -57,9 +58,28 @@ function changeColor(e){
     
     let arr= ["rgb(255, 0, 0)", "rgb(255, 165, 0)", "rgb(255, 255, 0)", "rgb(0, 128, 0)", "rgb(0, 0, 255)", "rgb(75, 0, 130)", "rgb(238, 130, 238)"];
 
-    let index = generateRandomNumber();
+    let color = e.target.style.backgroundColor;
+    
+    if(color == "rgb(255, 255, 255)"){
+        let index = generateRandomNumber();
+        e.target.style.backgroundColor = arr[index];
 
-    e.target.style.backgroundColor = arr[index];
+        color = arr[index];
+
+        if(!obj[color]){
+            obj[color] = 100;
+        }
+
+    }else{
+
+        color = e.target.style.backgroundColor;
+            
+        if(obj[color] > 0){
+            obj[color] = obj[color] - 10;
+            e.target.style.filter = `brightness(${obj[color]}%)`;
+        }
+    }
+
 
 }
 
